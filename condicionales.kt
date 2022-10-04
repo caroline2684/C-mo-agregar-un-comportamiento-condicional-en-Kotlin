5472e1a6a1f5f6497d036e248e05b6e6fc150db9
caroline2684 <79180738+caroline2684@users.noreply.github.com>
2022-10-03 23:03:47 -0500

    Create condicionales.kt

---
+++ condicionales.kt
@@ -0,0 +1,32 @@
+//Funcion if
+/*fun main() {
+   val num = 5
+   //verificamos si se cumple la condicion
+   if (num > 4) {
+       println("La variable es mayor que 4")
+   }
+}*/
+
+//Funcion if-else
+/*fun main() {
+    val num = 3
+    //si la condicion se cumple mostrara un mensaje, caso contrario mostrara otro
+    if (num > 4) {
+        println("La variable es mayor que 4")
+    } else {
+        println("La variable es menor que 4")
+    }
+}*/
+
+//funcion else-if
+fun main() {
+    val num = 4
+    //se crea un tercera opcion que puede ser cumplida
+    if (num > 4) {
+        println("The variable is greater than 4")
+    } else if (num == 4) {
+        println("The variable is equal to 4")
+    } else {
+        println("The variable is less than 4")
+    }
+}
